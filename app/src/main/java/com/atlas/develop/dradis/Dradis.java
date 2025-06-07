97b6dff1256d0ace22369cf48668bc85bd04517e
hafid <atlas-develop@proton.me>
2025-06-08 01:27:21 +0200

    gradle properties

--- app/src/main/java/com/atlas/develop/dradis/Dradis.java
+++ app/src/main/java/com/atlas/develop/dradis/Dradis.java
@@ -3,19 +3,8 @@
  */
 package com.atlas.develop.dradis;
 
-import org.example.list.LinkedList;
-
-import static org.example.utilities.StringUtils.join;
-import static org.example.utilities.StringUtils.split;
-import static org.example.app.MessageUtils.getMessage;
-
-import org.apache.commons.text.WordUtils;
-
 public class Dradis {
     public static void main(String[] args) {
-        LinkedList tokens;
-        tokens = split(getMessage());
-        String result = join(tokens);
-        System.out.println(WordUtils.capitalize(result));
+
     }
 }
