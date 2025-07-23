10f82f4f4bcb0ee1e4421b17a1a4f41c6acbd56e
Alice On Chain <alice.onchain@proton.me>
2025-07-23 16:03:16 +0200

    Update IpUtilsTest.java

--- src/test/java/com/atlas/develop/dradis/services/IpUtilsTest.java
+++ src/test/java/com/atlas/develop/dradis/services/IpUtilsTest.java
@@ -7,10 +7,10 @@ import java.net.UnknownHostException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class IpUtilsTest {
+class IpUtilsTest {
 
     @Test
-    public void testIpToBytes_ipv4() throws Exception {
+    void testIpToBytes_ipv4() throws Exception {
         String ipv4 = "123.45.67.89";
         byte[] expected = new byte[16];
         expected[10] = (byte) 0xFF;
@@ -25,7 +25,7 @@ public class IpUtilsTest {
     }
 
     @Test
-    public void testIpToBytes_ipv6() throws Exception {
+    void testIpToBytes_ipv6() throws Exception {
         String ipv6 = "2001:db8::1";
         byte[] expected = InetAddress.getByName(ipv6).getAddress();
         byte[] actual = IpUtils.ipToBytes(ipv6);
@@ -33,7 +33,7 @@ public class IpUtilsTest {
     }
 
     @Test
-    public void testIpToBytes_invalid() {
+    void testIpToBytes_invalid() {
         assertThrows(UnknownHostException.class, () -> {
             // adresse vide ou invalide
             IpUtils.ipToBytes("not-an-ip");
